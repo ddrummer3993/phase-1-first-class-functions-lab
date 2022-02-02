47b044091e38bd49e2f49396c69c0eeee17231ac
David Drummer <david.b.drummer@gmail.com>
2022-02-02 12:14:42 -0500

    commit tree fe8bcab44598b35133d4ebec8165c8f43c11f366

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+// create returnFirstTwoDrivers
+
+const returnFirstTwoDrivers = function (array) {
+    return array.slice(0, 2);
+};
+
+// create returnLastTwoDrivers
+
+const returnLastTwoDrivers = function (array) {
+    return array.slice(-2);
+};
+
+// create selectingDrivers array
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// create createFareMultiplier
+
+function createFareMultiplier (mult) {
+    return function (fare) {
+        return fare * mult;
+    };
+};
+
+//create fareDoubler()
+
+const fareTwoX = createFareMultiplier(2);
+function fareDoubler(fare) {
+    return fareTwoX(fare);
+};
+
+//create fareTripler
+
+const fareThreeX = createFareMultiplier(3);
+function fareTripler(fare) {
+    return fareThreeX(fare);
+};
+
+// create selectDifferentDrivers
+
+function selectDifferentDrivers(array, fn) {
+    return fn(array);
+};
